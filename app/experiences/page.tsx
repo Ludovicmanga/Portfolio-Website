57a1fd21380058d4503169299cb104abe558ac14
Ludovicmanga <ludovic.mangaj@gmail.com>
2023-10-13 14:35:44 +0200

    css improved

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import styles from "./page.module.css";
 import FormationOrExperienceBox from "../components/FormationOrExperienceBox/FormationOrExperienceBox";
-import dokinLogo from '@/public/dokinLogo.png'
-import goodboardLogo from '@/public/goodBoard.svg'
-import openClassRoomsLogo from '@/public/openClassroomsLogo.png'
-import iesegLogo from '@/public/iesegLogo.png'
+import dokinLogo from "@/public/dokinLogo.png";
+import goodboardLogo from "@/public/goodBoard.svg";
+import openClassRoomsLogo from "@/public/openClassroomsLogo.png";
+import iesegLogo from "@/public/iesegLogo.png";
 import { StaticImageData } from "next/image";
 
 type Props = {};
@@ -12,55 +12,80 @@ type Props = {};
 const page = (props: Props) => {
   const formationsAndExperiences: {
     img: StaticImageData;
+    imgHeight: number;
     title: string;
     companyName: string;
     from: string;
     to: string;
-    description: string;
+    description: ReactNode;
     type: "formation" | "expérience";
   }[] = [
     {
       img: dokinLogo,
+      imgHeight: 45,
       title: "CPO & développeur fullstack",
       companyName: "Dokin",
       from: "Mai 2022",
       to: "Mai 2023",
-      description:
-        "J'ai cofondé Dokin, ai été développeur fullstack et CPO, pendant 1 an. J'ai appliqué la méthode agile, avec ma casquette de CPO, et participé au développement des fonctionnalités, avec mon autre casquette de développeur",
+      description: (
+        <>
+          J&apos;ai cofondé Dokin, ai été développeur fullstack et CPO, pendant
+          1 an. J&apos;ai appliqué la méthode agile, avec ma casquette de CPO,
+          et participé au développement des fonctionnalités, avec mon autre
+          casquette de développeur
+        </>
+      ),
       type: "expérience",
     },
     {
       img: goodboardLogo,
+      imgHeight: 270,
       title: "Développeur fullstack",
       companyName: "Goodboard",
       from: "Mai 2022",
       to: "Aujourd'hui",
-      description:
-        "J'ai cofondé le projet Goodboard, avec Antoine, qui s'occupe du marketing. Je suis en charge de la partie développement du logiciel, et à tout ce qui touche à la technique",
+      description: (
+        <>
+          J&apos;ai cofondé le projet Goodboard, avec Antoine, qui s&apos;occupe
+          du marketing. Je suis en charge de la partie développement du
+          logiciel, et à tout ce qui touche à la technique
+        </>
+      ),
       type: "expérience",
     },
     {
       img: openClassRoomsLogo,
+      imgHeight: 170,
       title: "Développeur Web / Php symfony",
       companyName: "Openclassrooms",
       from: "Mai 2020",
       to: "Décembre 2021",
-      description:
-        "J'ai obtenu un diplôme de développeur web (équivalent bac+2 / +3) avec Openclassrooms. 20h de cours par semaine, et une dizaine de projets",
+      description: (
+        <>
+          J&apos;ai obtenu un diplôme de développeur web (équivalent bac+2 / +3)
+          avec Openclassrooms. 20h de cours par semaine, et une dizaine de
+          projets
+        </>
+      ),
       type: "formation",
     },
     {
       img: iesegLogo,
+      imgHeight: 90,
       title: "Master en management",
       companyName: "Ieseg",
       from: "Juillet 2013",
       to: "Juillet 2018",
-      description:
-        "J'ai obtenu mon master, dans l'école de commerce postback numéro 1",
+      description: (
+        <>
+          J&apos;ai obtenu mon master, dans l&apos;école de commerce postback
+          numéro 1
+        </>
+      ),
       type: "formation",
     },
   ];
-  
+
   return (
     <div className={styles.container}>
       <h1 className={styles.titleContainer}>Expériences</h1>
@@ -73,7 +98,7 @@ const page = (props: Props) => {
       {formationsAndExperiences
         .filter((formExp) => formExp.type === "formation")
         .map((formExp) => (
-          <FormationOrExperienceBox {...formExp}  key={formExp.companyName} />
+          <FormationOrExperienceBox {...formExp} key={formExp.companyName} />
         ))}
     </div>
   );
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,81 +1,127 @@
-import React from 'react'
-import FormationOrExperienceBox from '../components/FormationOrExperienceBox/FormationOrExperienceBox';
+import React, { ReactNode } from "react";
+import FormationOrExperienceBox from "../components/FormationOrExperienceBox/FormationOrExperienceBox";
 import reactLogo from "@/public/reactWithoutText.png";
 import nodeJsLogo from "@/public/nodejsLogo.png";
 import nextJsLogo from "@/public/nextjsLogo.png";
-import jsLogo from "@/public/javascriptLogo.png";
-import htmlLogo from "@/public/htmlLogo.png";
-import cssLogo from "@/public/cssLogo.png";
-import phpLogo from "@/public/php.png";
-import cLogo from "@/public/cLogo.png";
-import Image, { StaticImageData } from "next/image";
-import { Avatar, Paper, useMediaQuery } from "@mui/material";
-import dokinLogo from '@/public/dokinLogo.png'
-import goodboardLogo from '@/public/goodBoard.svg'
-import styles from './page.module.css'
+import { StaticImageData } from "next/image";
+import dokinPortfolioImg from "@/public/dokinPortfolioImg.png";
+import goodboardPortfolioImg from "@/public/goodboardPortfolioImg.png";
+import viciPortfolioImg from "@/public/viciPortfolioImg.png";
+import styles from "./page.module.css";
 
-const stackLogos = [
-  reactLogo,
-  nodeJsLogo,
-  nextJsLogo,
-  jsLogo,
-  htmlLogo,
-  cssLogo,
-  phpLogo,
-  cLogo,
-];
-
-type Props = {}
+type Props = {};
 
 const page = (props: Props) => {
+  const nextJsLogoWithHeight = {
+    heightBigScreen: 10,
+    heightSmallScreen: 10,
+    img: nextJsLogo,
+  };
+
+  const reactLogoWithHeight = {
+    heightBigScreen: 25,
+    heightSmallScreen: 25,
+    img: reactLogo,
+  };
+
+  const nodeJsLogoWithHeight = {
+    heightBigScreen: 25,
+    heightSmallScreen: 25,
+    img: nodeJsLogo,
+  };
 
   const projects: {
     img: any;
+    imgHeight: number;
     title: string;
-    description: string;
-    stack: StaticImageData[];
+    description: ReactNode;
+    stack?: {
+      heightBigScreen: number;
+      heightSmallScreen: number;
+      img: StaticImageData;
+    }[];
     githubOpen: boolean;
     githubLink: string;
     projectLink: string;
-    type: 'projet';
-}[] = [{
-    img: dokinLogo,
-    title: 'Dokin',
-    description: "Dokin est une extension à la suite Google Workspace permettant d\'automatiser les flux financiers des PME, en créant des liens entre les documents financiers et des sources de données diverses (CRM, réseaux sociaux, base de données...) via différentes API. Cette application a été réalisée avec un frontend en NextJs, et un backend sur Node Js (express). Le runtime de Google, Appscript a également été utilisée, nous permettant d'accéder à certaines fonctions et listeners indisponibles en dehors de ce dernier.",
-    stack: [nextJsLogo, reactLogo, nodeJsLogo],
-    githubLink: '',
-    githubOpen: false,
-    projectLink: 'https://dokin.co',
-    type: 'projet'
-  },
-  {
-    img: goodboardLogo,
-    title: 'Goodboard',
-    description: 'Goodboard est une application permettant à tout concepteur de logiciel de savoir ce que leurs utilisateurs souhaitent comme fonctionnalités. Ces derniers peuvent proposer des fonctionnalités, mais également voter pour des idées déjà proposées. les avis de ses utilisateurs sur les fonctionnalités souhaitées. Goodboard permet ensuite au concepteur de prendre des décisions grâce à ces données, notamment via des connexions API vers les logiciels de gestion, type Trello et Jira. Cette application a été créée en utilisant un backend Node Js (express) et un frontend React',
-    stack: [reactLogo, nodeJsLogo],
-    githubOpen: true,
-    githubLink: 'https://github.com/Ludovicmanga/GoodBoard',
-    projectLink: 'https://goodboard-app.herokuapp.com/login',
-    type: 'projet'
-  },
-  {
-    img: goodboardLogo,
-    title: 'Vici',
-    description: 'Vici est une application permettant à tout utilisateur de créer des memory cards, afin d\'assimiler de l`information des sujets, par le jeu. Les utilisateurs peuvent ainsi créer des cartes avec des questions et des réponses, puis tester sa mémoire, via des challenges',
-    stack: [nextJsLogo, reactLogo, nodeJsLogo],
-    githubOpen: true,
-    githubLink: 'https://github.com/Ludovicmanga/vici',
-    projectLink: 'https://vici-brown.vercel.app/',
-    type: 'projet'
-  }];
+    type: "projet";
+  }[] = [
+    {
+      img: dokinPortfolioImg,
+      imgHeight: 240,
+      title: "Dokin",
+      description: (
+        <>
+          Dokin est une extension à la suite Google Workspace permettant
+          d&apos;automatiser les flux financiers des PME, en créant des liens
+          entre les documents financiers et des sources de données diverses
+          (CRM, réseaux sociaux, base de données...) via différentes API. <br />
+          <br />
+          Cette application a été réalisée avec un frontend en NextJs, et un
+          backend sur Node Js (express). Le runtime de Google, Appscript a
+          également été utilisée, nous permettant d&apos;accéder à certaines
+          fonctions et listeners indisponibles en dehors de ce dernier.,
+        </>
+      ),
+      stack: [nextJsLogoWithHeight, reactLogoWithHeight, nodeJsLogoWithHeight],
+      githubLink: "",
+      githubOpen: false,
+      projectLink: "https://dokin.co",
+      type: "projet",
+    },
+    {
+      img: goodboardPortfolioImg,
+      imgHeight: 240,
+      title: "Goodboard",
+      description: (
+        <>
+          Goodboard est une application permettant à tout concepteur de logiciel
+          de savoir ce que leurs utilisateurs souhaitent comme fonctionnalités.
+          <br />
+          <br />
+          Ces derniers peuvent proposer des fonctionnalités, mais également
+          voter pour des idées déjà proposées. les avis de ses utilisateurs sur
+          les fonctionnalités souhaitées. Goodboard permet ensuite au concepteur
+          de prendre des décisions grâce à ces données, notamment via des
+          connexions API vers les logiciels de gestion, type Trello et Jira.
+          <br />
+          <br />
+          Cette application a été créée en utilisant un backend Node Js
+          (express) et un frontend React
+        </>
+      ),
+      stack: [reactLogoWithHeight, nodeJsLogoWithHeight],
+      githubOpen: true,
+      githubLink: "https://github.com/Ludovicmanga/GoodBoard",
+      projectLink: "https://goodboard-app.herokuapp.com/login",
+      type: "projet",
+    },
+    {
+      img: viciPortfolioImg,
+      imgHeight: 240,
+      title: "Vici",
+      description: (
+        <>
+          Vici est une application permettant à tout utilisateur de créer des
+          memory cards, afin d&apos;assimiler de l`information par le jeu. Les
+          utilisateurs peuvent ainsi créer des cartes avec des questions et des
+          réponses, puis tester sa mémoire, via des challenges
+        </>
+      ),
+      stack: [nextJsLogoWithHeight, reactLogoWithHeight, nodeJsLogoWithHeight],
+      githubOpen: true,
+      githubLink: "https://github.com/Ludovicmanga/vici",
+      projectLink: "https://vici-brown.vercel.app/",
+      type: "projet",
+    },
+  ];
 
   return (
     <div className={styles.container}>
-      { projects.map(project => (
+      {projects.map((project) => (
         <FormationOrExperienceBox {...project} key={project.title} />
-      )) }
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
